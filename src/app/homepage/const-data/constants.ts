40d74b14521728b7d3bbbb6102f59b8f8ec62034
zenjutahi <zenjutahi@gmail.com>
2023-06-07 20:58:02 +0300

    contants

---
+++ src/app/homepage/const-data/constants.ts
@@ -0,0 +1,52 @@
+export const CAR_OPTIONS = [
+  {"name": "3-Row Seat", "key": "RS" },
+  {"name": " A/C ", "key": "AC" },
+  {"name": "ABS", "key": "ABS" },
+  {"name": "Aero Parts", "key": "AP" },
+  {"name": "Aftermarket Alloy Wheels", "key": "AAW" },
+  {"name": "Aftermarket Meffler", "key": "AM" },
+  {"name": "Aftermarket Speaker", "key": "AS" },
+  {"name": "Airbag", "key": "A" },
+  {"name": "Alloy Wheels", "key": "AW" },
+  {"name": "Always Garaged", "key": "AG" },
+  {"name": "AM/FM", "key": "AF" },
+  {"name": "Anti-theft System", "key": "ATS" },
+  {"name": "Around View Camera ", "key": "AVC" },
+  {"name": "Audio System", "key": "AUS" },
+  {"name": "Back Camera ", "key": "BC" },
+  {"name": "Back Sensor ", "key": "BS" },
+  {"name": "Back Tire", "key": "BT" },
+  {"name": "Basic Equipment", "key": "BE" },
+  {"name": "Bench Seat", "key": "BS" },
+  {"name": "Body Kit", "key": "BK" },
+  {"name": "Box Truck ", "key": "BTR" },
+  {"name": "Bucket Seat", "key": "BKS" }
+]
+
+export const VEHICLE_DATA = [
+	{"model":"ILX", "make": "Acura", "year": [2022]},
+  {"model": "Integra", "make":"Acura", "year": [2024, 2023]},
+  {"model": "MDX", "make": "Acura", "year": [2024, 2023, 2022]},
+  {"model": "NSX", "make": "Acura", "year": [2022]},
+  {"model": "RDX", "make": "Acura", "year": [2023, 2022]},
+  {"model": "TLX", "make": "Acura", "year": [2023, 2022]},
+  {"model": "ZDX", "make": "Acura", "year": [2024]},
+  {"model": "Giulia", "make": "Alfa Romeo", "year": [2023, 2022]},
+  {"model": "Stelvio", "make": "Alfa Romeo", "year": [2023, 2022]},
+  {"model": "Tonale", "make": "Alfa Romeo", "year": [2024, 2023]},
+  {"model": "DB11", "make": "Aston Martin", "year": [2023, 2022]},
+  {"model": "DBS", "make": "Aston Martin", "year": [2023, 2022]},
+  {"model": "DBX", "make": "Aston Martin", "year": [2024, 2023, 2022]},
+  {"model": "Vantage", "make": "Aston Martin", "year": [2023, 2022]},
+  {"model": "A3", "make":"Audi", "year": [2023, 2022]},
+  {"model": "A4", "make": "Audi", "year": [2023, 2022]},
+  {"model": "A4 allroad", "make": "Audi", "year": [2023, 2022]},
+  {"model":"i4","make": "BMW", "year":[2023, 2022]},
+  {"model": "i7", "make":"BMW","year": [2024, 2023]},
+  {"model": "iX", "make": "BMW", "year": [2024, 2023]},
+  {"model": "M2", "make": "BMW", "year": [2023]}
+
+]
+
+
+
