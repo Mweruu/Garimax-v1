8e11aced7809bda2a851074cba8e7f52cf687186
Mweru Mwangi <violettamweru213@gmail.com>
2023-06-09 13:07:24 +0300

    preview

--- src/app/homepage/vehicles/uploadcar/preview/preview.component.ts
+++ src/app/homepage/vehicles/uploadcar/preview/preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-preview',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
   sidebarVisible3!: boolean;
+  previewForm!:FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.previewForm = this.fb.group({
+      make:['',Validators.required],
+      model:['',Validators.required],
+      yearOfManufacture:['',Validators.required],
+      price:['',Validators.required],
+      location:['',Validators.required],
+      mileage:['',Validators.required],
+      image:['',Validators.required],
+      fueltype:['', Validators.required],
+      vehicleType:['', Validators.required],
+      transmission:['', Validators.required],
+      color:['', Validators.required],
+      steering:['', Validators.required],
+      engineSize:['', Validators.required],
+      drivetype:['',Validators.required],
+      description:['']
+    })
   }
 
 }
